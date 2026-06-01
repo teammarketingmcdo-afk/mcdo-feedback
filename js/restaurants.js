af4296f4c802a4a9a2dc719de7ff238ce6c43167
teammarketingmcdo-afk <team.marketing.mcdo@gmail.com>
2026-06-01 16:03:09 +0200

    Modification feedback email

--- js/restaurants.js
+++ js/restaurants.js
@@ -10,7 +10,7 @@ tiktok: "https://www.tiktok.com/@mcdo.meriadeck?lang=fr"
 
 },
 
-"Sortie 9": {
+"Sortie 9 rue Magudas": {
 
 google: "https://www.google.com/search?sca_esv=3bd1b57b106fee9e&cs=0&sxsrf=ANbL-n6n6kB2U8Q06iYGEvHMrzICl0cNBA:1780305162495&q=McDonald%27s+Avis&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDcxMTMzNzQzMDExsbAwMjY2Nd3AyPiKkd832SU_LzEnRb1YwbEss3gRK7oIAGQpRPo_AAAA&rldimm=17446671604448823355&tbm=lcl&hl=fr-FR&sa=X&ved=2ahUKEwjDgrTG2eWUAxW8nycCHZJlBk4Q9fQKegQIGhAG&biw=1528&bih=732&dpr=1.25#lkt=LocalPoiReviews&lrd=0xd54d765ba54a831:0xf21f07634811083b,3,,,,",
 
@@ -20,7 +20,7 @@ tiktok: "https://www.tiktok.com/@mcdo.s9.merignac?lang=fr"
 
 },
 
-"Sortie 10": {
+"Sortie 10 rue des Châtaigners": {
 
 google: "https://www.google.com/search?sca_esv=3bd1b57b106fee9e&cs=0&sxsrf=ANbL-n4MYj0bwPoNRP5Ejm6WRbpH5jYOlA:1780305118938&q=McDonald%27s+Avis&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDAxNTc3MDQ3MjU1szA1NbAwtdzAyPiKkd832SU_LzEnRb1YwbEss3gRK7oIAMERGuk_AAAA&rldimm=10457701725568550859&tbm=lcl&hl=fr-FR&sa=X&ved=2ahUKEwiX39Gx2eWUAxWufKQEHWmgOo8Q9fQKegQIGRAG&biw=1528&bih=732&dpr=1.25#lkt=LocalPoiReviews&lrd=0xd54d9fbd7c39dab:0x9121384fe780c7cb,3,,,,",
 
@@ -30,7 +30,7 @@ tiktok: "https://www.tiktok.com/@mcdo.s10.merignac?lang=fr"
 
 },
 
-"Sortie 11": {
+"Sortie 11 rue Hipparque": {
 
 google: "https://www.google.com/search?sca_esv=3bd1b57b106fee9e&cs=0&sxsrf=ANbL-n5iDHvsccskS3ISuxRQfcAWow3e0Q:1780304934479&q=McDonald%27s+Avis&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDE1Nja3MDY1MTUwtDQzMzI3M97AyPiKkd832SU_LzEnRb1YwbEss3gRK7oIAOQLecw_AAAA&rldimm=14533783545019662763&tbm=lcl&hl=fr-FR&sa=X&ved=2ahUKEwink9fZ2OWUAxUvUqQEHZzeFGQQ9fQKegQIEhAG&biw=1528&bih=732&dpr=1.25#lkt=LocalPoiReviews&lrd=0xd54db000a219441:0xc9b25f21835081ab,3,,,,",
 
