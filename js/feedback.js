ebe244df42cbc3fd7a954af07e934c038cab68a7
teammarketingmcdo-afk <team.marketing.mcdo@gmail.com>
2026-06-01 16:18:28 +0200

    css change

--- js/feedback.js
+++ js/feedback.js
@@ -3,3 +3,23 @@ const rating = localStorage.getItem("rating") || "Non renseigné";
 
 document.getElementById("restaurantInput").value = restaurant;
 document.getElementById("ratingInput").value = rating;
+
+const contactChoice =
+document.getElementById("contactChoice");
+
+const contactFields =
+document.getElementById("contactFields");
+
+contactChoice.addEventListener("change", () => {
+
+    if(contactChoice.value === "Oui"){
+
+        contactFields.style.display = "block";
+
+    }else{
+
+        contactFields.style.display = "none";
+
+    }
+
+});
