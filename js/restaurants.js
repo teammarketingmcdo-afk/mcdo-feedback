fde1328d3fae4ef1e2851bafdabeb1204968fadc
teammarketingmcdo-afk <team.marketing.mcdo@gmail.com>
2026-06-03 12:02:31 +0200

    update

--- js/restaurants.js
+++ js/restaurants.js
@@ -8,7 +8,7 @@ tiktok:"https://www.tiktok.com/@mcdo.meriadeck?lang=fr"
 
 },
 
-"Sortie 9 rue Magudas":{
+"Sortie 9 Rue Magudas":{
 
 google:"https://g.page/r/CTsIEUhjBx_yEBM/review",
 instagram:"https://www.instagram.com/mcdo.s9.merignac/?hl=fr",
@@ -16,7 +16,7 @@ tiktok:"https://www.tiktok.com/@mcdo.s9.merignac?lang=fr"
 
 },
 
-"Sortie 10 rue des Châtaigniers":{
+"Sortie 10 Rue des Châtaigniers":{
 
 google:"https://g.page/r/CcvHgOdPOCGREBM/review",
 instagram:"https://www.instagram.com/mcdo.s10/?hl=fr",
@@ -24,7 +24,7 @@ tiktok:"https://www.tiktok.com/@mcdo.s10.merignac?lang=fr"
 
 },
 
-"Sortie 11 rue Hipparque":{
+"Sortie 11 Rue Hipparque":{
 
 google:"https://g.page/r/CauBUIMhX7LJEBM/review",
 instagram:"https://www.instagram.com/mcdo.s11.villagedecathlon/?hl=fr",
