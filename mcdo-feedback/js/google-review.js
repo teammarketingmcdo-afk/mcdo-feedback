806c43b4df442438e23e3fcb7e9f3c8121b93b3f
teammarketingmcdo-afk <team.marketing.mcdo@gmail.com>
2026-06-22 15:59:13 +0200

    Delete mcdo-feedback directory

--- mcdo-feedback/js/google-review.js
+++
@@ -1,9 +0,0 @@
-const restaurant = localStorage.getItem("restaurant");
-
-const googleReviewBtn = document.getElementById("googleReviewBtn");
-
-if(restaurant && restaurantsData[restaurant]){
-    googleReviewBtn.href = restaurantsData[restaurant].google;
-}else{
-    googleReviewBtn.href = "index.html";
-}
